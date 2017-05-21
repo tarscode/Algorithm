7976435f9f561c9b4bcaab802dc189ae51c78545
tarsdroid <tarsdroid@hotmail.com>
2017-05-21 10:43:20 +0800

    增加题目

--- src/baidu/test1/Main.java
+++ src/baidu/test1/Main.java
@@ -1,14 +1,14 @@
 package baidu.test1;
 import java.util.Iterator;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.TreeSet;
 /**
  * 【工程】: Algorithm 包名: baidu.test1 类名: Main
  * 【作者】: Tars
  * 【时间】: 2017/4/27 0027 18:45
- * 【题目】:
- * 【内容】:
+ * 【题目】: 买帽子
+ * 【内容】: 度度熊想去商场买一顶帽子，商场里有N顶帽子，有些帽子的价格可能相同。
+ *          度度熊想买一顶价格第三便宜的帽子，问第三便宜的帽子价格是多少？
  * 【版本】: V1.0
  * 【运行时间】:
  * 【时间复杂度】:
